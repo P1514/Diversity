0f10ecf71864dc07df46d1f7e300d7a5f427a4f2
P1514 <guinunogois@gmail.com>
2016-07-22 10:44:07 +0100

    V1.0.4 Hotfix

--- Diversity/src/importDB/PSS.java
+++ Diversity/src/importDB/PSS.java
@@ -1,6 +1,12 @@
 package importDB;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.Vector;
+
+import backend.Settings;
 
 //Post individual Object
 public class PSS {
@@ -15,5 +21,25 @@ public class PSS {
 	
 	public int getTag(String word){
 		return tags.get(word);
+	}
+	
+	public void importPSS(){
+		BufferedReader inputFile = null;
+		Vector<String> names = new Vector<String>();
+		try {
+        	inputFile = new BufferedReader( new FileReader( Settings.DATA_FOLDER + "/" + AppConst.FILENAME_PRODUCTS ) );
+            String line = "";
+            do {
+                line = this.inputFile.readLine();
+                if (line != null) {
+                	names.add( line );
+                }
+            } while( line != null );
+        }
+        catch(IOException ex){
+            System.out.println("File IO Exception: " + ex.getMessage());
+        }
+		this.closeInputFile();
+		
 	}
 }
