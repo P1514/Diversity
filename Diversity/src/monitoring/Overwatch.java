5e65079d1ef2b8dae6337f08a12051b2b874737d
P1514 <guinunogois@gmail.com>
2016-11-04 16:15:12 +0000

    V2.1

---
+++ Diversity/src/monitoring/Overwatch.java
@@ -0,0 +1,12 @@
+package monitoring;
+
+import java.util.TimerTask;
+
+public class Overwatch extends TimerTask{
+
+	@Override
+	public void run() {
+		// TODO This is the method that run at 00:00 each 24h to update everything
+		
+	}
+}
