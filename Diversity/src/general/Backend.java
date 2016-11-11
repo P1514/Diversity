a679b9ac9dd7003fe8b767bafdf237595b2af276
P1514 <guinunogois@gmail.com>
2016-11-11 16:36:25 +0000

    V2.3

--- Diversity/src/general/Backend.java
+++ Diversity/src/general/Backend.java
@@ -68,10 +68,6 @@ public class Backend {
 			switch (op) {
 			case 22:
 				return Roles.getRestrictions(msg.getString("Role")).toString();//TODO função retorna JSONARRAy ).toString();
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/master
  			case 21:
 				return GetProducts.getTree().toString();
 
