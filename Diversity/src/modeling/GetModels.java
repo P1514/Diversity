098932c6f18462dc86abef3075e83860a163e60a
P1514 <guinunogois@gmail.com>
2016-11-18 17:55:26 +0000

    V2.3.1

--- Diversity/src/modeling/GetModels.java
+++ Diversity/src/modeling/GetModels.java
@@ -64,7 +64,7 @@ public class GetModels {
 		//obj.put("Gender", model.getGender());
 		String Products = new String();
 		for(String a : model.getProducts().split(",")){
-			Products+=Data.productdb.get(Long.valueOf(a)).get_Name()+";";
+			Products+=a+";";
 		}
 		obj.put("Final_products", Products);
 		obj.put("Archive", model.getArchived());
