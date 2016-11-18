eec625c13da97d7b50cc48dd99491601ce23c87b
P1514 <guinunogois@gmail.com>
2016-11-18 17:13:35 +0000

    V2.3.1

--- Diversity/Local.sql
+++ Diversity/Local.sql
@@ -1,8 +1,10 @@
--- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+CREATE DATABASE  IF NOT EXISTS `sentimentanalysis` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `sentimentanalysis`;
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
 --
 -- Host: localhost    Database: sentimentanalysis
 -- ------------------------------------------------------
--- Server version	5.7.13-log
+-- Server version	5.7.16-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -285,5 +287,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-
--- Dump completed on 2016-11-18 16:33:04
+-- Dump completed on 2016-11-18 17:13:22
