770739dbffb92151d5e77793a140c1a6a9c82d28
P1514 <guinunogois@gmail.com>
2017-02-15 09:55:02 +0000

    New adjectives

--- Diversity/src/general/Adjectives.java
+++ Diversity/src/general/Adjectives.java
@@ -18,6 +18,7 @@ public final class Adjectives {
 	public Adjectives() {
 		adjectives = new ConcurrentHashMap<String, Double>();
 		adjectives.put("phenomenal", 97.0);
+		adjectives.put("flexible",		96.0);
 		adjectives.put("world-class", 95.0);
 		adjectives.put("incredible", 92.0);
 		adjectives.put("exceptional", 90.0);
@@ -25,6 +26,7 @@ public final class Adjectives {
 		adjectives.put("amazing", 87.0);
 		adjectives.put("terrific", 85.0);
 		adjectives.put("astonishing", 83.0);
+		adjectives.put("quite flexible", 82.0);
 		adjectives.put("wonderful", 81.0);
 		adjectives.put("superior", 80.0);
 		adjectives.put("fantastic", 79.0);
@@ -34,6 +36,7 @@ public final class Adjectives {
 		adjectives.put("fabulous", 75.0);
 		adjectives.put("splendid", 74.0);
 		adjectives.put("astounding", 73.0);
+		adjectives.put("supportive", 72.0);
 		adjectives.put("magnificent", 72.0);
 		adjectives.put("outstanding", 71.0);
 		adjectives.put("sensational", 70.0);
@@ -44,11 +47,14 @@ public final class Adjectives {
 		adjectives.put("popular", 65.0);
 		adjectives.put("sweet", 63.0);
 		adjectives.put("fine", 61.0);
+		adjectives.put("very comfy", 60.0);
 		adjectives.put("cool", 59.0);
 		adjectives.put("okay", 57.0);
 		adjectives.put("fair", 55.0);
+		adjectives.put("comfy", 52.0);
 		adjectives.put("average", 50.0);
 		adjectives.put("mediocre", 46.0);
+		adjectives.put("easily wrinkled",42.0);
 		adjectives.put("lame", 40.0);
 		adjectives.put("substandard", 39.0);
 		adjectives.put("limited", 38.0);
@@ -60,15 +66,18 @@ public final class Adjectives {
 		adjectives.put("miserable", 32.0);
 		adjectives.put("nasty", 31.0);
 		adjectives.put("bad", 30.0);
+		adjectives.put("rigid", 30.0);
 		adjectives.put("poor", 29.0);
 		adjectives.put("pitiful", 28.0);
 		adjectives.put("pathetic", 27.0);
 		adjectives.put("appalling", 26.0);
+		adjectives.put("very rigid", 25.0);
 		adjectives.put("horrible", 24.0);
 		adjectives.put("dreadful", 22.0);
 		adjectives.put("terrible", 20.0);
 		adjectives.put("awful", 15.0);
 		adjectives.put("abysmal", 13.0);
+		adjectives.put("stone rigid", 	12.0);
 	}
 
 	/**
