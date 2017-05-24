2b850b4381f566a8800d2fa590b2bc0140dc3ff3
Fábio Mano <f.mano@campus.fct.unl.pt>
2017-05-24 18:28:21 +0100

    FM (#102)
    * Connection fix
    
    * Tooltip delay
    
    * Post-meeting fixes
    
    * Ignore classpath file
    
    * Logging change
    
    Uses DB and file system
    
    * Fixed delete model message(now displays model name)
    
    * Snapshot page now sends JSON
    
    * Snapshot page fix
    
    * Snapshot change name to ID
    
    * Endpoint URL fix
    
    * Tests and new tag cloud (not done)
    
    * Lean Rules endpoint
    
    * JSESSIONID Generation and chart linked zoom
    
    * Chart zoom rollback
    
    * Added time to log table
    
    * Lean rules endpoint
    
    * Lean rules - Polarity still missing
    
    * Lean rules - untested
    
    * Lean rules refactor
    
    * Lean rules done
    
    * KPI Endpoints

--- Diversity/src/endpoints/GetLeanRules.java
+++ Diversity/src/endpoints/GetLeanRules.java
@@ -6,10 +6,15 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
@@ -21,8 +26,15 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import com.sun.media.jfxmedia.logging.Logger;
+
+import general.Data;
+import general.Settings;
+import extraction.Globalsentiment;
 
 @Path("/getLeanRules")
 public class GetLeanRules {
@@ -30,7 +42,13 @@ public class GetLeanRules {
 	private static final String ENDPOINT = "https://www.khira.it/LeanDesignRules/LDRServices/designProject_leanRule";
 	private static final String DP_PARAMETER = "design_project_id";
 	private static final String VALIDATED_PARAMETER = "validated";
-	
+
+	private Map<Integer, ArrayList<Integer>> matrix;
+	private Map<Integer, Integer> designProjectSentiment;
+	private Connection cnlocal;
+	private int numDp;
+	private int numRules;
+
 	@DefaultValue("")
 	@QueryParam("design_project_id") // design projects
 	String dp;
@@ -38,11 +56,11 @@ public class GetLeanRules {
 	UriInfo ui;
 
 	/**
-	 * NOT DONE, NEED ENDPOINT TO RETURN ALL DESIGN PROJECTS
-	 * Builds a matrix where the rows are all the rules in the selected design project and 
-	 * the columns are the design projects with those rules. Matrix contains the sentiment
-	 * value for each of those (rule,project) pairs.
-	 * @return
+	 * Builds a matrix where the rows are all the rules in the selected design
+	 * project and the columns are the design projects with those rules. Still
+	 * need to include the polarity value of each rule.
+	 * 
+	 * @return - a JSON string with the lean rule matrix
 	 * @throws JSONException
 	 */
 	@GET
@@ -50,57 +68,192 @@ public class GetLeanRules {
 	public Response welcome() throws JSONException {
 		if ("".equals(dp))
 			return Response.status(Response.Status.BAD_REQUEST).build();
-		int id;
-		try {
-			id = Integer.parseInt(dp);
-		} catch (NumberFormatException e) {
-			return Response.status(Response.Status.BAD_REQUEST).build();
-		}
-		
-	    return Response.status(Response.Status.OK).entity(getRules(id, true)).build();
-	}
-	
-	/**
-	 * Request all design projects (no endpoint yet, so the data is simulated)
-	 * @return - a list of all design projects
-	 */
-	private List<Integer> getAllDesignProjects() {
-		List<Integer> tmp = new ArrayList<Integer>();
-		tmp.add(17);
-		tmp.add(18);
-		tmp.add(19);
-		return tmp;
+
+		buildMatrix();
+
+		JSONArray json = buildResult();
+
+		return Response.status(Response.Status.OK).entity(json.toString()).build();
 	}
 
-	private String getRules(int id, boolean validated) {
-		String jsonString = "";
+	private void dbconnect() {
 		try {
-			URL url = new URL(ENDPOINT + "?" + DP_PARAMETER + "=" + id + "&" + VALIDATED_PARAMETER + "=" + validated);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setRequestMethod("GET");
-			conn.setRequestProperty("Accept", "application/json");
-			
-			if (conn.getResponseCode() != 200) {
-				throw new RuntimeException("HTTP error " + conn.getResponseCode());
-			}
-			
-			BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
-			
-			String tmp;
-			while((tmp = in.readLine()) != null) {
-				jsonString += tmp;
-			}
-			
-			in.close();
-			conn.disconnect();
-			
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
+			cnlocal = Settings.connlocal();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		return jsonString;
 	}
-	
+
+	private void buildMatrix() {
+		// get design project rules
+		List<Integer> rules = getRules();
+		List<Integer> designProjects = null;
+		matrix = new HashMap<Integer, ArrayList<Integer>>();
+		// for each rule get the design projects where it is active
+		for (int r : rules) {
+			designProjects = getDesignProjects(r);
+
+			for (int p : designProjects) {
+				List<Integer> tmp;
+				if (!matrix.containsKey(r)) {
+					tmp = new ArrayList<Integer>();
+					tmp.add(p);
+					matrix.put(r, (ArrayList<Integer>) tmp);
+				} else {
+					tmp = matrix.get(r);
+					tmp.add(p);
+					matrix.replace(r, (ArrayList<Integer>) tmp);
+				}
+			}
+		}
+	}
+
+	private JSONArray buildResult() throws JSONException {
+		JSONArray json = new JSONArray();
+
+		Set<Integer> rulesSet = matrix.keySet();
+
+		for (int r : rulesSet) {
+			JSONObject obj = new JSONObject();
+			obj.put("Rule", r);
+			obj.put("Projects", matrix.get(r));
+
+			List<Double> dpSentiment = new ArrayList<Double>();
+
+			for (int dp : matrix.get(r)) {
+				if (dp != -1) {
+					dpSentiment.add(getDesignProjectSentiment(dp));
+				}
+			}
+			double avg = 0;
+			for (double s : dpSentiment) {
+				avg += s;
+			}
+
+			avg = avg / dpSentiment.size();
+
+			int res = -1;
+
+			if (avg > 50) {
+				res = 1;
+			} else if (avg == 50) {
+				res = 0;
+			} else {
+				res = -1;
+			}
+			obj.put("Polarity", res);
+			json.put(obj);
+		}
+
+		return json;
+	}
+
+	private ArrayList<Integer> getRules() {
+		List<Integer> rules = new ArrayList<Integer>();
+
+		String select = "SELECT lean_rule_id FROM diversity_common_repository.design_project_rule WHERE design_project_id =?";
+
+		PreparedStatement query1 = null;
+		try {
+			dbconnect();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			query1 = cnlocal.prepareStatement(select);
+			query1.setInt(1, Integer.parseInt(dp));
+			try (ResultSet rs = query1.executeQuery()) {
+				while (rs.next()) {
+					rules.add(rs.getInt(1));
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			cnlocal.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return (ArrayList<Integer>) rules;
+	}
+
+	private ArrayList<Integer> getDesignProjects(int ruleId) {
+		List<Integer> designProjects = new ArrayList<Integer>();
+
+		String select = "SELECT design_project_id FROM diversity_common_repository.design_project_rule WHERE lean_rule_id =? AND checked = 1";
+
+		PreparedStatement query1 = null;
+		try {
+			dbconnect();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			query1 = cnlocal.prepareStatement(select);
+			query1.setInt(1, ruleId);
+			try (ResultSet rs = query1.executeQuery()) {
+				while (rs.next()) {
+					designProjects.add(rs.getInt(1));
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			cnlocal.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return (ArrayList<Integer>) designProjects;
+	}
+
+	private double getDesignProjectSentiment(int dpId) throws JSONException {
+		List<Integer> models = new ArrayList<Integer>();
+		List<Integer> frequencies = new ArrayList<Integer>();
+		List<Double> sentiments = new ArrayList<Double>();
+		String select = "SELECT " + Settings.lmtable_id + "," + Settings.lmtable_update + " FROM " + Settings.lmtable
+				+ "  WHERE " + Settings.lmtable_designproject + "=?";
+
+		PreparedStatement query1 = null;
+		try {
+			dbconnect();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			query1 = cnlocal.prepareStatement(select);
+			query1.setInt(1, dpId);
+			try (ResultSet rs = query1.executeQuery()) {
+				while (rs.next()) {
+					models.add(rs.getInt(1));
+					frequencies.add(rs.getInt(2));
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			cnlocal.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		for (int i = 0; i < models.size(); i++) {
+			Globalsentiment gs = new Globalsentiment();
+			JSONArray sentiment = models.get(i) != -1 ? gs.getAvgSentiment((String) null, (String) null, models.get(i))
+					: null;
+			sentiments.add(sentiment != null ? sentiment.getJSONObject(0).getDouble("Value") : -1);
+		}
+		double avg = 0;
+		sentiments.removeIf(i -> i == -1);
+
+		for (double s : sentiments) {
+			avg += s;
+		}
+
+		return avg / sentiments.size();
+	}
 }
