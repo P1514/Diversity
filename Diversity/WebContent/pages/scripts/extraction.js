6aedde85d03de52b36f2c907a8730d4828bf1546
fmano <f.mano@campus.fct.unl.pt>
2017-10-31 17:01:47 +0000

    Removed extrapolation from snapshots

--- Diversity/WebContent/pages/scripts/extraction.js
+++ Diversity/WebContent/pages/scripts/extraction.js
@@ -220,6 +220,7 @@ function connect() {
 			$('#agefilt').hide();
 			$('#locationfilt').hide();
 			$('#finalfilt').hide();
+			$('#extrapolate_label').hide();
 		}
 
 		// If Op is 'Error', display the server message in an overlay window
