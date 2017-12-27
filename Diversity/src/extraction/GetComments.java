d1ceeb9c04a64287378e18957b98239668c2dd8e
Guilherme Gois <guilhermegois@Guilhermes-MBP.lan>
2017-12-27 11:48:00 +0000

    Get Comments Fix
    Using same method as top 5

--- Diversity/src/extraction/GetComments.java
+++ Diversity/src/extraction/GetComments.java
@@ -3,6 +3,7 @@ package extraction;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -10,6 +11,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import extraction.GetPosts.Post_gp;
 import general.Backend;
 import general.Logging;
 import general.Settings;
@@ -54,13 +56,12 @@ public class GetComments {
 
 	public JSONArray getAll(JSONObject msg) throws JSONException {
 		JSONArray result = new JSONArray();
-		String[] preresult = new String[10000];
+		ArrayList<Post_gp> pre_result = new ArrayList<>();
 		JSONObject obj = new JSONObject();
 		obj.put("Op", "comments");
 		result.put(obj);
 		String insert;
-		int ntops = 0;
-		insert = "Select " + Settings.latable_name + "," + Settings.latable_influence + "," + Settings.latable_location
+		insert = "Select p."+Settings.lptable_id+"," + Settings.latable_name + "," + Settings.latable_influence + "," + Settings.latable_location
 				+ "," + Settings.latable_gender + "," + Settings.latable_age + "," + Settings.lptable_polarity + ","
 				+ Settings.lptable_message + " from " + Settings.lptable + " p left join " + Settings.latable + " a on "+Settings.lptable_authorid+"=a."+Settings.latable_id+" where ( ("
 				+ "p." + Settings.lptable_id + "=? OR"
@@ -74,16 +75,29 @@ public class GetComments {
 			query1.setString(2, msg.getString("Values"));
 
 			try (ResultSet rs = query1.executeQuery()) {
-
-				for (int i = 0;; i++) {
-					if (!rs.next())
-						break;
-					ntops++;
-					preresult[i] = rs.getString(Settings.latable_name) + ",," + rs.getDouble(Settings.latable_influence)
-							+ ",," + rs.getString(Settings.latable_location) + ",,"
-							+ rs.getString(Settings.latable_gender) + ",," + rs.getInt(Settings.latable_age) + ",,";
-					preresult[i] += rs.getDouble(Settings.lptable_polarity) + ",,";
-					preresult[i] += rs.getString(Settings.lptable_message) + " ";
+				Post_gp temp_post = new Post_gp();
+				if(!rs.next() || rs.getLong(Settings.lptable_id) != Long.parseLong(msg.getString("Values"))) {
+				}else {
+					temp_post.name=rs.getString(Settings.latable_name);
+					temp_post.influence = rs.getDouble(Settings.latable_influence);
+					temp_post.location = rs.getString(Settings.latable_location);
+					temp_post.gender=rs.getString(Settings.latable_gender);
+					temp_post.age=rs.getInt(Settings.latable_age);
+					temp_post.polarity=rs.getDouble(Settings.lptable_polarity);
+					temp_post.message=rs.getString(Settings.lptable_message);
+					
+				}
+				pre_result.add(temp_post);
+				while(rs.next()) {
+					temp_post=new Post_gp();
+					temp_post.name=rs.getString(Settings.latable_name);
+					temp_post.influence = rs.getDouble(Settings.latable_influence);
+					temp_post.location = rs.getString(Settings.latable_location);
+					temp_post.gender=rs.getString(Settings.latable_gender);
+					temp_post.age=rs.getInt(Settings.latable_age);
+					temp_post.polarity=rs.getDouble(Settings.lptable_polarity);
+					temp_post.message=rs.getString(Settings.lptable_message);
+					pre_result.add(temp_post);
 				}
 
 			} catch (Exception e) {
@@ -93,20 +107,33 @@ public class GetComments {
 			LOGGER.log(Level.SEVERE, Settings.err_unknown, e);
 		}
 
-		for (int i = 0; i < ntops; i++) {
+		for (Post_gp answer : pre_result) {
 			obj = new JSONObject();
-			String[] preresults = preresult[i].split(",,");
-			obj.put("Name", preresults[0]);
-			obj.put("Influence", trunc(preresults[1]));
-			obj.put("Location", preresults[2]);
-			obj.put("Gender", preresults[3]);
-			obj.put("Age", preresults[4]);
-			obj.put("Polarity", trunc(preresults[5]));
-			obj.put("Message", preresults[6]);
+			obj.put("Name", answer.name);
+			obj.put("Influence", trunc(answer.influence+""));
+			obj.put("Location", answer.location);
+			obj.put("Gender", answer.gender);
+			obj.put("Age", answer.age);
+			obj.put("Polarity", trunc(answer.polarity+""));
+			obj.put("Message", answer.message);
 			result.put(obj);
 
 		}
 		return result;
+
+	}
+	
+	class Post_gp{
+		public String name = "No Name";
+		public double influence = 0.0;
+		public String location = "No Location";
+		public String gender = "No Gender";
+		public int age = 0;
+		public Long date = (long)0;
+		public Double polarity = 0.0;
+		public Double reach = 0.0;
+		public Integer comments = 0;
+		public String message = "No Message";
 
 	}
 
